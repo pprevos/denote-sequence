d5233c971fbf077dd987521a12083b3c2d933b0b
Protesilaos Stavrou <info@protesilaos.com>
2025-11-19 12:14:54 +0200

    Account for empty string in denote-sequence-dired

--- denote-sequence.el
+++ denote-sequence.el
@@ -1109,7 +1109,7 @@ For a more specialised case, see `denote-sequence-find-relatives-dired'."
        nil))))
   (pcase-let* ((relative-p (denote-has-single-denote-directory-p))
                (files-fn `(lambda ()
-                            (let* ((files (if ,prefix
+                            (let* ((files (if (and prefix (not (string-blank-p prefix)))
                                               (denote-sequence-get-all-files-with-prefix ,prefix)
                                             (denote-sequence-get-all-files)))
                                    (files-with-depth (if ,depth
